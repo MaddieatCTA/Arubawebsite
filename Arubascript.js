2f582ddffaf7336d8a592a46b75b7633e9a4b0d4
Madeleine Morris <m.morris@corecomtechacademy.co.uk>
2023-08-16 13:47:09 +0100

    last

---
+++ Arubascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#btn').click(function(){
+       alert("Aruba says hello back!");
+    });
+});
+
+$(document).ready(function(){
+    $('#mendhi').click(function(){
+       alert("This is some mendhi Aruba has done.");
+    });
+});
+
+$(document).ready(function(){
+    $('#pool').click(function(){
+       alert("This is Aruba winning bronze at BUCS for pool.");
+    });
+});
+
+$(document).ready(function(){
+    $('#art').click(function(){
+       alert("some art Aruba has done.");
+    });
+});
+
+$(document).ready(function(){
+    $('#makeup').click(function(){
+       alert("a makeup look done by and on Aruba.");
+    });
+});
